2034f52acded6bb002469c31644b3f8521576e60
mbt142 <mbt6298@gmail.com>
2025-07-27 22:10:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,6 @@ yesBtn.addEventListener('click', () => {
   document.querySelector('.container').innerHTML = `
     <h1>💍 Yes! I Love You Too 💖</h1>
     <img src="https://media.tenor.com/qQwFcVmIbY0AAAAC/gojo-hug.gif" alt="hug" width="300" style="border-radius: 16px; margin-top: 20px;"/>
-    <p style="margin-top: 20px; font-size: 1.2em;">Your forever — MBT 🌌</p>
+    <p style="margin-top: 20px; font-size: 1.2em;">Your forever — VENKATESH 🌌</p>
   `;
 });
